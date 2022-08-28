64a956cca650f7571fde864bcdd413a2bc48d145
Rios <rodrigosr.rios@live.com>
2022-08-28 14:25:03 -0300

    Script provisionar servidor web

---
+++ script-apache.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+echo 'Atualizando o servidor...'
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo 'Baixando e copiando os arquivos da aplicação...'
+cd /tmp
+wget <link dos arquivos>
+unzip <nome do arquivo zip>
+cd <caminho>
+cp -R *  var/www/html/
+
+
+
+
+
+
